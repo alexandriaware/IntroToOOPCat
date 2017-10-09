e834e1b985e82b40fec88333d487d59ee909fe62
alexandriaware <ali.mercer92@gmail.com>
2017-10-09 13:46:46 -0400

    made dog bark

--- IntroToOOPCat/Dog.cs
+++ IntroToOOPCat/Dog.cs
@@ -8,5 +8,80 @@ namespace IntroToOOPCat
 {
     class Dog
     {
+        //fields
+        private string furLength;
+        private float height;
+        private string runningSpeed;
+        private double weight;
+
+
+
+
+        //Properties
+        public string FurLength
+        {
+            get { return this.furLength; }
+            set { this.furLength = value; }
+
+        }
+
+        public float Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public string RunningSpeed // <-- this is a new property, thus the Pascal case, which it allows me to access the field runningSpeed.
+        {
+            get { return this.runningSpeed; }
+            set { this.runningSpeed = value; }
+        }
+
+        public double Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+
+
+
+        //Constructors
+        public Dog() // <--this is our class 
+        {
+            //default constructor
+        }
+
+        //overloaded Constructor
+        public Dog(string furLength, float height)
+        {
+            this.furLength = furLength;
+            this.height = height;
+        }
+
+
+        //Methods
+        public void Run()
+        {
+            runningSpeed = "getting faster.";
+            weight--;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("\a\a\a\a\a\a\a\a\a\a");
+        }
+
+        public void Potty()
+        {
+            weight -= 0.1;
+        }
+
+        public void Groom()
+        {
+            furLength = "All cleaned up.";
+        }
+
+
     }
 }
