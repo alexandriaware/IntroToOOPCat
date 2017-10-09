6e8fe3b69ef1a47791f1db02917b3d488cc08a93
alexandriaware <ali.mercer92@gmail.com>
2017-10-09 11:13:03 -0400

    Initial Commit

---
+++ IntroToOOPCat/Cat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOPCat
+{
+    class Cat
+    {
+        //STATES
+        //fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //Properties
+        public string Name
+        {
+            get { return this.name; }
+            //you need a new get/set for every variable age and furColor
+            set { this.name = value; }
+        }
+
+
+        //BEHAVIORS 
+        //Constructors
+        public Cat()
+        {
+        //default constructor has to have empty parentheses
+        //but it can have stuff in the curly braces like 
+        //this.name = name;
+        }
+
+        public Cat(string name, int age, string furColor) //overloaded constructor
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+
+
+        //Methods
+        public void Eat()
+        {
+            if (isHungry == true)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is the cat hungry? " + isHungry);
+        }
+       
+        public void WorkOut()
+        {
+            if(isHungry == false)
+            {
+                isHungry = true;
+            }
+            Console.WriteLine("The cat worked out. Hungry now? " + isHungry);
+        }
+
+    }
+}
---
+++ IntroToOOPCat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOPCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat mittens = new Cat();
+
+            mittens.Name = "Mittens";
+
+            Cat secondCat = new Cat("Oz", 3, "orange and white");
+
+            Console.WriteLine(mittens.Name);
+            Console.WriteLine(secondCat.Name);
+
+            mittens.Eat(); //<--in this one, mittens IS hungry, so it eats, and now he's not hungry.
+            mittens.Eat(); //<-- in this one, mittens ISN'T even hungry so it doesn't do anything.
+            mittens.WorkOut(); 
+
+        }
+    }
+}
